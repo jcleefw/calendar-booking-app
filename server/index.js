f3e5fedf60c7806aff28c4d074ab1ace1e9688e9
JC Lee <jc.lee@marketplacer.com>
2022-03-13 01:09:49 +1100

    chore(packages): install packages
    - @chakra-ui/layout and dependencies
    - momentjs
    - add classnames
    - add uuid

--- server/index.js
+++ server/index.js
@@ -1,20 +1,22 @@
-const express = require('express')
-const cors = require('cors')
-const fs = require('fs')
+const express = require('express');
+const cors = require('cors');
+const fs = require('fs');
+const { v4: uuidv4 } = require('uuid');
 
-const app = express()
-app.use(cors()) // so that app can access
+const app = express();
+app.use(cors()); // so that app can access
 
 const bookings = JSON.parse(fs.readFileSync('./server/bookings.json')).map(
   (bookingRecord) => ({
+    id: uuidv4(),
     time: Date.parse(bookingRecord.time),
     duration: bookingRecord.duration * 60 * 1000, // mins into ms
     userId: bookingRecord.user_id,
-  }),
-)
+  })
+);
 
 app.get('/bookings', (_, res) => {
-  res.json(bookings)
-})
+  res.json(bookings);
+});
 
-app.listen(3001)
+app.listen(3001);
